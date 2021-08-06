15ad65becde47a9eed294b7d42ca12378b73333f
Nadya Zykova <xilda777@gmail.com>
2021-08-06 22:45:22 +0400

    First commit

---
+++ src/test/java/web/page/DashboardPage.java
@@ -0,0 +1,15 @@
+package web.page;
+
+import com.codeborne.selenide.*;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DashboardPage {
+    private SelenideElement heading = $("[data-test-id=dashboard]");
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
+    }
+}
---
+++ src/test/java/web/test/MoneyTransferTest.java
@@ -0,0 +1,51 @@
+package web.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import web.data.DataHelper;
+import web.page.DashboardReplenishCards;
+import web.page.DashboardYourCards;
+import web.page.LoginPageV1;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MoneyTransferTest {
+    @BeforeEach
+    public void setUpAll() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPageV1();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV1() {
+        var cardsInfo = DataHelper.getCardsInfo();
+        var replenishThis = new DashboardReplenishCards();
+        var yourCards = new DashboardYourCards();
+        int firstBalanceBefore = yourCards.getFirstCardBalance();
+        int secondBalanceBefore = yourCards.getSecondCardBalance();
+        int difference = 200;
+        yourCards.replenishFirst();
+        replenishThis.replenish(Integer.toString(difference), cardsInfo, 1);
+        assertEquals(firstBalanceBefore + difference, yourCards.getFirstCardBalance());
+        assertEquals(secondBalanceBefore - difference, yourCards.getSecondCardBalance());
+    }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV2() {
+        var cardsInfo = DataHelper.getCardsInfo();
+        var replenishThis = new DashboardReplenishCards();
+        var yourCards = new DashboardYourCards();
+        int firstBalanceBefore = yourCards.getFirstCardBalance();
+        int secondBalanceBefore = yourCards.getSecondCardBalance();
+        int difference = 200;
+        yourCards.replenishSecond();
+        replenishThis.replenish(Integer.toString(difference), cardsInfo, 2);
+        assertEquals(firstBalanceBefore - difference, yourCards.getFirstCardBalance());
+        assertEquals(secondBalanceBefore + difference, yourCards.getSecondCardBalance());
+    }
+}
+
